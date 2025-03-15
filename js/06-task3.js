32497a751c24d2be8a4c5cfb02fea5709b183edc
HannaAssaf <145717934+HannaAssaf@users.noreply.github.com>
2025-03-15 21:37:32 +0000

    bvcuhvb

--- js/06-task3.js
+++ js/06-task3.js
@@ -19,3 +19,9 @@ const house = {
 
 const perimeter = calculateHousePerimeter(house);
 console.log(`Периметр будинку: ${perimeter}`);
+const house = {
+  sideA: 10,
+  sideB: 15,
+  sideC: 10,
+  sideD: 15,
+};
