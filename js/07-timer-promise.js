93a8129dc03ae831c2edeeb47d8409dee9c3e8b4
HannaAssaf <145717934+HannaAssaf@users.noreply.github.com>
2025-03-26 22:32:46 +0000

    xfvdff

--- js/07-timer-promise.js
+++ js/07-timer-promise.js
@@ -433,3 +433,102 @@ function showNotification() {
 function hideNotification() {
   notification.classList.remove('is-visible');
 }
+const NOTIFICATION_DELAY = 3000;
+let timeoutId = null;
+const notification = document.querySelector('.js-alert');
+
+notification.addEventListener('click', onNotificationClick);
+showNotification();
+
+/**
+ * Функції
+ */
+function onNotificationClick() {
+  // console.log("Click on notification");
+  hideNotification();
+  clearTimeout(timeoutId);
+}
+
+function showNotification() {
+  notification.classList.add('is-visible');
+
+  timeoutId = setTimeout(() => {
+    console.log(
+      'Закриваємо сповіщення автоматично, щоб воно не залишалося відкритим'
+    );
+    hideNotification();
+  }, NOTIFICATION_DELAY);
+}
+
+function hideNotification() {
+  notification.classList.remove('is-visible');
+}
+const NOTIFICATION_DELAY = 3000;
+let timeoutId = null;
+const notification = document.querySelector('.js-alert');
+
+notification.addEventListener('click', onNotificationClick);
+showNotification();
+
+/**
+ * Функції
+ */
+function onNotificationClick() {
+  // console.log("Click on notification");
+  hideNotification();
+  clearTimeout(timeoutId);
+}
+
+function showNotification() {
+  notification.classList.add('is-visible');
+
+  timeoutId = setTimeout(() => {
+    console.log(
+      'Закриваємо сповіщення автоматично, щоб воно не залишалося відкритим'
+    );
+    hideNotification();
+  }, NOTIFICATION_DELAY);
+}
+
+function hideNotification() {
+  notification.classList.remove('is-visible');
+}
+function onNotificationClick() {
+  // console.log("Click on notification");
+  hideNotification();
+  clearTimeout(timeoutId);
+}
+
+function showNotification() {
+  notification.classList.add('is-visible');
+
+  timeoutId = setTimeout(() => {
+    console.log(
+      'Закриваємо сповіщення автоматично, щоб воно не залишалося відкритим'
+    );
+    hideNotification();
+  }, NOTIFICATION_DELAY);
+}
+
+function hideNotification() {
+  notification.classList.remove('is-visible');
+}function onNotificationClick() {
+  // console.log("Click on notification");
+  hideNotification();
+  clearTimeout(timeoutId);
+}
+
+function showNotification() {
+  notification.classList.add('is-visible');
+
+  timeoutId = setTimeout(() => {
+    console.log(
+      'Закриваємо сповіщення автоматично, щоб воно не залишалося відкритим'
+    );
+    hideNotification();
+  }, NOTIFICATION_DELAY);
+}
+
+function hideNotification() {
+  notification.classList.remove('is-visible');
+}
